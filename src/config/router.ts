40cca39edbf809ac0044a8793e342a82ee83300b
Daniel Santos De Sousa <daniel.s.6140@gmail.com>
2023-10-17 15:45:06 -0300

    Fixing redirect

--- src/config/router.ts
+++ src/config/router.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import store from './store'
 import { useRouter } from 'vue-router'
 import { SetUserLogout } from '../config/store'
@@ -47,7 +47,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory('/Ollix-Frontend/'),
     routes,
 })
 
