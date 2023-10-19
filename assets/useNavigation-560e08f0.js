76bb030bdc451e064db6aabd0e7cdf9661964c85
DaniFTT <64164438+DaniFTT@users.noreply.github.com>
2023-10-19 04:32:57 +0000

    Deploying to gh-pages from  @ f911466514e0764d7eff5c8ba9d70824ebc71699 🚀

--- assets/useNavigation-560e08f0.js
+++
@@ -1,1 +0,0 @@
-import{u,C as a}from"./index-68c32551.js";function s(){const o=u(),t=a();return{goBack:()=>{t.fullPath!=="/auth"&&o.go(-1)}}}export{s as u};
---
+++ assets/useNavigation-7fd3c0b2.js
@@ -0,0 +1,1 @@
+import{u,x as a}from"./index-56bca0b2.js";function s(){const o=u(),t=a();return{goBack:()=>{t.fullPath!=="/auth"&&o.go(-1)}}}export{s as u};
